723bc1a155d5f052691ce202964706047299b3de
liuchang5454 <liuchang5454@gmail.com>
2018-10-07 22:56:38 -0600

    finish the client side code. The previous bug of fetching messages is found, the bug was on the server side.

--- src/components/MessageItem.js
+++ src/components/MessageItem.js
@@ -2,25 +2,34 @@ import React from "react";
 import Moment from "react-moment";
 import {Link} from "react-router-dom";
 import DefaultProfileImg from "../images/default-profile-image.jpg";
+import { removeMessage } from "../store/actions/messages";
 
-const MessageItem = ({data, profileImageUrl, text, username}) => (
+const MessageItem = ({data, profileImageUrl, text, username, removeMessage, isCorrectUser}) => (
   <div>
-    <img 
-      src={profileImageUrl || DefaultProfileImg}
-      alt={username}
-      height="100"
-      width="100"
-      className="timeline-image"
-    />
-    <div className="message-area">
-      <Link to="/">@{username} &nbsp;</Link>
-      <span className="text-muted">
-        <Moment className="text-muted" format="Do MMM YYYY">
-          {data}
-        </Moment>
-      </span>
-      <p>{text}</p>
-    </div>
+    <li className="list-group-item">
+      <img 
+        src={profileImageUrl || DefaultProfileImg}
+        alt={username}
+        height="100"
+        width="100"
+        className="timeline-image"
+      />
+      <div className="message-area">
+        <Link to="/">@{username} &nbsp;</Link>
+        <span className="text-muted">
+          <Moment className="text-muted" format="Do MMM YYYY">
+            {data}
+          </Moment>
+        </span>
+        <p>{text}</p>
+        {isCorrectUser && (
+          <a className="btn btn-danger" onClick={removeMessage}>
+            Delete
+          </a>  
+        )}
+      </div>
+    </li>
+    
   </div>
 );
 
---
+++ src/components/UserAside.js
@@ -0,0 +1,20 @@
+import React from "react";
+import DefaultProfileImg from "../images/default-profile-image.jpg";
+
+const UserAside = ({profileImageUrl, username}) => (
+  <aside className="col-sm-2">
+    <div className="panel panel-default">
+      <div className="panel-body">
+        <img 
+          src={profileImageUrl || DefaultProfileImg}
+          alt={username}
+          width="200px"
+          height="200px"
+          className="img-thumbnail"
+        />
+      </div>
+    </div>
+  </aside>
+);
+
+export default UserAside;
